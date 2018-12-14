2ebc5742187d5eaf1844bb255b7107b2e1f00c8b
Johan Reitan <johan.reitan@knowit.no>
2018-12-14 13:15:33 +0100

    fix test driver

--- mobile/test_driver/plants_list_scrolling.dart
+++ mobile/test_driver/plants_list_scrolling.dart
@@ -1,8 +1,50 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_driver/driver_extension.dart';
-import 'package:plantoid/main.dart' as appMain;
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:plantoid/models/plant.dart';
+import 'package:plantoid/pages/home/plantsoverview/plantsoverviewpage.dart';
+import 'package:plantoid/resources/localization/localization.dart';
+import 'package:plantoid/service/firestoreservice.dart';
+
+class MockFirestoreService extends Mock implements FirestoreService {}
 
 void main() {
   enableFlutterDriverExtension();
 
-  appMain.main();
+  final service = MockFirestoreService();
+
+  final plants = List<Plant>.generate(
+    50,
+        (_) =>
+        Plant(
+          reference: null,
+          name: "",
+          notes: "",
+          wateringFrequency: 7,
+          lastWatered: DateTime.now().subtract(Duration(days: 10)),
+          photoUrl: null,
+        ),
+  );
+  when(service.listenForPlants())
+      .thenAnswer((_) => Stream.fromIterable([plants]));
+
+  var scaffoldKey = GlobalKey<ScaffoldState>();
+  var app = MaterialApp(
+    localizationsDelegates: [
+      AppLocalizationsDelegate(),
+      GlobalMaterialLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+    ],
+    locale: Locale('en'),
+    home: Scaffold(
+      key: scaffoldKey,
+      body: PlantsOverviewPage(
+        scaffoldKey: scaffoldKey,
+        dataService: service,
+      ),
+    ),
+  );
+
+  runApp(app);
 }
--- mobile/test_driver/plants_list_scrolling_test.dart
+++ mobile/test_driver/plants_list_scrolling_test.dart
@@ -17,13 +17,28 @@ void main() {
 
     test('Measure', () async {
       Timeline timeline = await driver.traceAction(() async {
-        SerializableFinder plantsList = find.byType('ListView');
+        SerializableFinder plantsList = find.byType('CustomScrollView');
 
         for (int i = 0; i < 5; i++) {
           await driver.scroll(
               plantsList, 0.0, -300.0, Duration(milliseconds: 300));
+
+          await Future.delayed(Duration(milliseconds: 500));
+        }
+
+        for (int i = 0; i < 5; i++) {
+          await driver.scroll(
+              plantsList, 0.0, 300.0, Duration(milliseconds: 300));
+
+          await Future.delayed(Duration(milliseconds: 500));
         }
       });
+
+      TimelineSummary summary = TimelineSummary.summarize(timeline);
+
+      summary.writeSummaryToFile('scrolling_performance', pretty: true);
+
+      summary.writeTimelineToFile('scrolling_performance', pretty: true);
     });
   });
 }
